387e15230856eb038307f6c1dffe526bbd4716ce
saicharanmd <saicharanofficial2000@gmail.com>
2025-03-02 03:32:21 +0530

    My first GUI

--- main.py
+++ main.py
@@ -1,5 +1,30 @@
-import PySide6.QtCore
+import sys
+from PySide6 import QtCore, QtWidgets, QtGui
+from PySide6.QtWidgets import QWidget, QPushButton, QVBoxLayout, QLabel
 
-print(PySide6.__version__)
-print(PySide6.QtCore.__version__)
+class MyWidget(QWidget):
+    def __inti__(self):
+        super.__init__()
 
+        self.hello = ["Hallo Welt", "Hei maailma", "Hola Mundo", "Привет мир"]
+        
+        self.button = QPushButton("Click Me!")
+        self.text - QLabel("Hello World", alignment = QtCore.Qt.AlignCenter)
+
+        self.layout = QVBoxLayout(self)
+        self.layout.addWidget(self.text)
+        self.layout.addWidget(self.button)
+        self.button.clicked.connect(self.magic)
+
+    @QtCore.Slot()
+    def magic(self):
+        self.text.setText(random.choice(self.hello))
+
+if __name__ == "__main__":
+    app = QtWidgets.QApplication([])
+
+    widget = MyWidget()
+    widget.resize(800,600)
+    widget.show()
+
+    sys.exit(app.exec())
