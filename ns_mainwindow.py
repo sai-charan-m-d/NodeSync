a925db3e4774aba8c00d6e47478aa003beb2f967
saicharanmd <saicharanofficial2000@gmail.com>
2025-03-02 20:53:17 +0530

    Added resizable window

--- ns_mainwindow.py
+++ ns_mainwindow.py
@@ -1,11 +1,35 @@
-from PySide6.QtWidgets import QApplication, QMainWindow 
-from PySide6.QtGui import QIcon
+from PySide6.QtWidgets import QApplication, QMainWindow , QStatusBar
+from PySide6.QtGui import QIcon, QAction
 
 class MainWindow(QMainWindow):
     def __init__(self, app):
         super().__init__()
-        
+
         self.app = app
         self.setWindowTitle("NodeSync")
         self.icon = QIcon("icons/nodesync.png")
         self.setWindowIcon(self.icon)
+        
+        width = 800
+        height = 600
+        self.setMinimumSize(width, height)
+
+        #creating the menu bar
+        menubar = self.menuBar()
+
+        #File Menu 
+        file_menu = menubar.addMenu("&File")
+        #Actions in the File Menu
+        create_project_action = file_menu.addAction("New Project")
+        load_project_action = file_menu.addAction("Load Project")
+        file_menu.addSeparator()
+
+        #Quit App
+        quit_action = file_menu.addAction("Quit")
+        quit_action.triggered.connect(self.quit_app)
+
+        #statusBar
+        self.setStatusBar(QStatusBar(self))
+
+    def quit_app(self):
+        self.app.quit()
