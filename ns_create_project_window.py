eadceb470216a4a626c6a8e5c4ddb9172ee69daa
M D SAI CHARAN <45945187+sai-charan-m-d@users.noreply.github.com>
2025-03-06 15:24:59 +0530

    Added working folder creation at a specified location

--- ns_create_project_window.py
+++ ns_create_project_window.py
@@ -1,6 +1,8 @@
-from PySide6.QtWidgets import QWidget,QVBoxLayout,QLabel,QFileDialog,QPushButton,QLineEdit,QHBoxLayout,QTextEdit
+from PySide6.QtWidgets import QWidget,QVBoxLayout,QLabel,QFileDialog,QPushButton,QLineEdit,QHBoxLayout,QTextEdit,QMessageBox
 from PySide6.QtCore import Qt
 import os
+import json
+
 #from PySide6.QtCore import 
 #from PySide6.QtGui import
 
@@ -18,9 +20,9 @@ class CreateProjectWindow(QWidget):
         #Project Name Layout
         project_name_layout = QHBoxLayout()
         project_name_label = QLabel("Project Name")
-        project_name = QLineEdit()
+        self.project_name = QLineEdit()
         project_name_layout.addWidget(project_name_label)
-        project_name_layout.addWidget(project_name)
+        project_name_layout.addWidget(self.project_name)
 
         #Project Location Layout
         project_location_layout = QHBoxLayout()
@@ -35,7 +37,9 @@ class CreateProjectWindow(QWidget):
         #Button Layout
         button_layout = QHBoxLayout()
         create_button = QPushButton("Create Project")
+        create_button.setToolTip("Creates the folder structure")
         cancel_button = QPushButton("Cancel")
+        cancel_button.setToolTip("Cancel the Folder Creation")
         button_layout.addWidget(create_button)
         button_layout.addWidget(cancel_button)
         create_button.clicked.connect(self.create_project_folders)
@@ -53,11 +57,40 @@ class CreateProjectWindow(QWidget):
     def choose_dir(self):
         dir_path = QFileDialog.getExistingDirectory(self, "Select the Directory")
         self.project_location_textbox.setText(str(dir_path))
+
+    def load_folder_structure(self):
+        with open("E:/01_WORK/01_PROJ/NODESYNC/NodeSync/folder_structure.json","r") as f:
+            return json.load(f)
     
     def create_project_folders(self):
         dir_path = self.project_location_textbox.text()
-        os.path.join(dir_path)
-        print(os.getcwd())
+        project_name_text = self.project_name.text()
+
+        if not dir_path:
+            QMessageBox.warning(self, "Error", "No Directory Selected")
+            return
+        
+        folder_structure = self.load_folder_structure()
+
+        #creating the project folder
+
+        os.makedirs(os.path.join(dir_path,project_name_text), exist_ok=True)
+        project_folder = dir_path + "/" + project_name_text
+
+        try:
+            for main_folder, sub_folders in folder_structure.items():
+                main_folder_path = os.path.join(project_folder, main_folder)
+                os.makedirs(main_folder_path, exist_ok=True)
+
+                for sub_folder in sub_folders:
+                    os.makedirs(os.path.join(main_folder_path, sub_folder), exist_ok=True)
+            
+            QMessageBox.information(self, "Success", "Project Created at:\n{dir_path}")
+        except Exception as e:
+            QMessageBox.critical(self, "Error", f"Failed to create folders:\n{e}")
+        
+        self.close()
     
     def quit_widget(self):
+        QMessageBox.information(self, "Cancelled", "Project creation cancelled")
         self.close()
