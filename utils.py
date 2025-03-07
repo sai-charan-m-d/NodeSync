ed63d9b664bb7f1dca765ff6d2879e9dd624dfd4
M D SAI CHARAN <45945187+sai-charan-m-d@users.noreply.github.com>
2025-03-07 23:13:12 +0530

    Added generate metadata function

--- utils.py
+++ utils.py
@@ -1,4 +1,4 @@
-import os
+import os, json, datetime
 from pathlib import Path
 
 def folder_exists(folder_path):
@@ -11,9 +11,27 @@ def project_exists(project_name, project_folder):
         return False
 
 def folder_name_exists(folder_name:str, parent_dir_path:str):
-    path = Path(parent_dir_path) / folder_name
+    path = Path(parent_dir_path+'/'+folder_name)
     
     if path.exists():
         return True
     else:
         return False
+
+def generate_meta_data(project_name, project_path, folder_structure):
+    metadata = {
+        "name" : project_name,
+        "path" : project_path,
+        "created_date" : datetime.datetime.now().strftime("%y-%m-%d %H:%M:%S"),
+        "folders": list(folder_structure.keys())
+
+    }
+    metadata_file = os.path.join(project_path,"metadata.json")
+
+    try:
+        with open(metadata_file, "w") as f:
+            json.dump(metadata,f,indent=4)
+        return metadata
+    except Exception as e:
+        print(f"Error writing metadata file : {e}")
+        return None
