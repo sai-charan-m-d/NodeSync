d95b4ceb8ff40ac2c4d187d50cc8a0da520da71e
M D SAI CHARAN <45945187+sai-charan-m-d@users.noreply.github.com>
2025-03-07 22:02:24 +0530

    Added few more Untility Funcitons

--- utils.py
+++ utils.py
@@ -1,4 +1,19 @@
 import os
+from pathlib import Path
 
-def folder_exists(fodler_path):
-    return os.path.exits(fodler_path) and os.path.isdir(folder_path)
+def folder_exists(folder_path):
+    return os.path.exits(folder_path) and os.path.isdir(folder_path)
+
+def project_exists(project_name, project_folder):
+    if project_name in project_folder:
+        return True
+    else:
+        return False
+
+def folder_name_exists(folder_name:str, parent_dir_path:str):
+    path = Path(parent_dir_path) / folder_name
+    
+    if path.exists():
+        return True
+    else:
+        return False
