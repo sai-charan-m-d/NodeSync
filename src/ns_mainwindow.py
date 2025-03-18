b050dcecee596f3bb42195e72a536b8c4a320424
M D SAI CHARAN <45945187+sai-charan-m-d@users.noreply.github.com>
2025-03-18 19:48:51 +0530

    renamed the centralWidget to project_list_widget

--- src/ns_mainwindow.py
+++ src/ns_mainwindow.py
@@ -1,5 +1,6 @@
 from PySide6.QtWidgets import QApplication, QMainWindow , QStatusBar
 from PySide6.QtGui import QIcon, QAction
+from PySide6.QtCore import Qt
 from src.ns_create_project_window import CreateProjectWindow
 from src.ns_list_widget import ProjectListWidget
 from src.ns_settings_window import SettingsWindowWidget
@@ -16,18 +17,19 @@ class MainWindow(QMainWindow):
         width = 800
         height = 600
         self.setMinimumSize(width, height)
-        central_widget = ProjectListWidget()
-        self.setCentralWidget(central_widget)
+        self.project_list_widget = ProjectListWidget()
+        self.setCentralWidget(self.project_list_widget)
 
         #creating the menu bar
         menubar = self.menuBar()
 
         #File Menu 
         file_menu = menubar.addMenu("&File")
-        
+
         #Actions in the File Menu
         create_project_action = file_menu.addAction("New Project")
         create_project_action.triggered.connect(self.create_project_launch)
+        #create_project_action.toolTip("Create's a new project and adds it to the list")
         load_project_action = file_menu.addAction("Load Project")
         file_menu.addSeparator()
 
@@ -47,7 +49,7 @@ class MainWindow(QMainWindow):
         self.app.quit()
     
     def create_project_launch(self):
-        self.widget = CreateProjectWindow(self)
+        self.widget = CreateProjectWindow(self, self.project_list_widget)
         self.widget.show()
         
     def list_widget(self):
@@ -57,3 +59,23 @@ class MainWindow(QMainWindow):
     def settings_launch(self):
         self.widget = SettingsWindowWidget()
         self.widget.show()
+
+    # Mouse Events to move the Widget
+    def mousePressEvent(self, event):
+        """Detects mouse click and stores the offset."""
+        if event.button() == Qt.LeftButton:
+            self.dragging = True  
+            self.offset = event.globalPosition().toPoint() - self.pos() 
+            event.accept()
+
+    def mouseMoveEvent(self, event):
+        """Moves the window while dragging."""
+        if self.dragging:
+            self.move(event.globalPosition().toPoint() - self.offset) 
+            event.accept()
+
+    def mouseReleaseEvent(self, event):
+        """Stops dragging when mouse button is released."""
+        if event.button() == Qt.LeftButton:
+            self.dragging = False 
+            event.accept()
